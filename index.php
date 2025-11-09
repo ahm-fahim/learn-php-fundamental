91dfd4c75fe3194cfe32fe39e87b11cb910ad426
ahm-fahim <contactahmfahim@gmail.com>
2025-11-09 13:02:15 +0600

    variable and variable scope , comment

--- index.php
+++ index.php
@@ -5,7 +5,50 @@
 <h1>My first PHP page</h1>
 
 <?php
-echo "Hello World!";
+
+// variable and variable scope , comment
+echo "Hello World! ";
+echo phpversion();
+echo "<br>";
+
+$color = "red";
+echo "My favorite color is $color ";
+echo "<br>";
+
+$number = 10 ;
+
+var_dump($color);
+echo "<br>";
+var_dump($number);
+echo "<br>";
+
+var_dump(5);
+echo "<br>";
+var_dump("John");
+echo "<br>";
+var_dump(3.14);
+echo "<br>";
+var_dump(true);
+echo "<br>";
+var_dump([2, 3, 56]);
+echo "<br>";
+var_dump(NULL);
+echo "<br>";
+
+
+
+$a = 10;
+$b = 20;
+function myFunc(){
+    $food = "orange";
+    global $a, $b;
+    echo $food;
+    echo "<br>";
+    echo $a + $b;
+}
+
+myFunc();
+
 ?>
 
 </body>
